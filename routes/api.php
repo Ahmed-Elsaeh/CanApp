d1a46898cf494dbf448d60df8b491378dafca5ae
Ahmed-Elsaeh <seee7aaa@gmail.com>
2023-03-13 16:37:47 +0200

    New auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,12 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware('cors')->group(function () {
-
-    Route::post('/user/register', 'API\UserController@register');
-    Route::post('/user/login', 'API\UserController@login');
-    Route::get('/user/logout', 'API\UserController@logout');
-});
+Route::post('/auth/register', [AuthController::class, 'createUser']);
+Route::post('/auth/login', [AuthController::class, 'loginUser']);
